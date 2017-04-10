229fd781edb6ef4441a590b07d2692fe803a4e8a
nushicoco <nushicarmon@gmail.com>
2017-04-10 17:31:54 +0300

    custom validations for the component, refs example in inner component

--- client/src/containers/profile.js
+++ client/src/containers/profile.js
@@ -6,15 +6,40 @@ import { connect } from 'react-redux';
 
 class Profile extends Component
 {
+    constructor() {
+        super();
+        this.state = {userName:"coco2"};
+    }
+    update (e) {
+        this.setState({
+            userName:this.userName.refs.input.value
+        });
+    }
+
     render () {
-        console.log ("props.userName",this.props);
+        // console.log ("props.userName",this.props);
 
         return (
             <div>
-                <h1>cococo</h1>
+                <h1>Name is {this.state.userName}</h1>
+                <MySecondInput ref={ component => this.userName = component} update={this.update.bind(this)}/>
+                <Button>Save</Button>
             </div>)
     }
 }
+
+class MySecondInput extends Component {
+    render(){
+        return (<input ref="input" onChange={this.props.update}/>)
+    }
+}
+
+const MyInput = (props) =>
+    <input onChange={props.update}/>
+
+const Button = (props) =>
+    <button onChange={props.update}>{props.children}</button>
+
 
 // function mapStateToProps(state) {
 //     return {
@@ -23,7 +48,14 @@ class Profile extends Component
 // }
 
 Profile.propTypes = {
-    userName:React.PropTypes.string
+    userName:React.PropTypes.string,
+
+    // Custom validations your component requires
+    email(props, propName, component){
+        if (!(propName in props)){
+            return new Error(`missing ${propName}`)
+        }
+    }
 }
 
 Profile.defualtProps = {
