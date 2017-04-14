013ee66d0dd14f04804af5d116f4f608b9ccf32d
nushicoco <nushicarmon@gmail.com>
2017-04-14 21:02:36 +0300

    removed old navigation and added react-bootstrap

--- client/src/containers/navigation_bar.js
+++ client/src/containers/navigation_bar.js
@@ -2,7 +2,8 @@
  * Created by einavcarmon on 12/04/2017.
  */
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+// import { connect } from 'react-redux';
 // import '../../public/lib/ie.css';
 // import '../../public/lib/styles.css';
 
@@ -46,37 +47,34 @@ class NavigationBar extends Component
 
         return (
             <div>
-        <header>
-            <a href="#home" className="logo" data-scroll>Fixed Nav</a>
-            <nav className="nav-collapse">
-                <ul>
-                    <li className="menu-item active"><a href="#home" data-scroll>Home</a></li>
-                    <li className="menu-item"><a href="#about" data-scroll>About</a></li>
-                    <li className="menu-item"><a href="#projects" data-scroll>Projects</a></li>
-                    <li className="menu-item"><a href="#blog" data-scroll>Blog</a></li>
-                    <li className="menu-item"><a href="http://www.google.com" target="_blank">Google</a></li>
-                </ul>
-            </nav>
-        </header>
+                <Navbar inverse collapseOnSelect>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="#">React-Bootstrap</a>
+                        </Navbar.Brand>
+                        <Navbar.Toggle />
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+                        <Nav>
+                            <NavItem eventKey={1} href="#">Link</NavItem>
+                            <NavItem eventKey={2} href="#">Link</NavItem>
+                            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                                <MenuItem eventKey={3.1}>Action</MenuItem>
+                                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                                <MenuItem divider />
+                                <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                            </NavDropdown>
+                        </Nav>
+                        <Nav pullRight>
+                            <NavItem eventKey={1} href="#">Link Right</NavItem>
+                            <NavItem eventKey={2} href="#">Link Right</NavItem>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+        );
 
-        <section id="home">
-            <h1>Fixed Nav</h1>
-        <p>The code and examples are hosted on GitHub and can be <a href="https://github.com/adtile/fixed-nav">found from here</a>. Read more about the approach from&nbsp;<a href="http://blog.adtile.me/2014/03/03/responsive-fixed-one-page-navigation/">our&nbsp;blog</a>.</p>
-        </section>
-
-        <section id="about">
-            <h1>About</h1>
-            </section>
-
-            <section id="projects">
-            <h1>Projects</h1>
-            </section>
-
-            <section id="blog">
-            <h1>Blog</h1>
-            </section></div>
-
-    )
     }
 }
 
@@ -103,11 +101,11 @@ NavigationBar.propTypes = {
     userName:React.PropTypes.string,
 
     // Custom validations your component requires
-    email(props, propName, component){
-        if (!(propName in props)){
-            return new Error(`missing ${propName}`)
-        }
-    }
+    // email(props, propName, component){
+    //     if (!(propName in props)){
+    //         return new Error(`missing ${propName}`)
+    //     }
+    // }
 }
 
 NavigationBar.defualtProps = {
